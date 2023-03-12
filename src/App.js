1887e1704e424812f2bb2e7f8400961b2b487bd6
Bryce McMath (GitKraken) <bryce.j.mcmath@gmail.com>
2023-03-11 17:56:23 -0800

    chore: update content, clean up unused

--- src/App.js
+++ src/App.js
@@ -1,90 +1,104 @@
-import './App.scss';
+import "./App.scss";
 import Reveal from "react-awesome-reveal";
-import { fadeFromLeft, fadeFromRight } from './helpers/animations';
-import maximus from './assets/maximus.jpeg';
-import softUI from './assets/soft-ui.gif';
-import lighthouse from './assets/lighthouse.jpeg';
-import traderu from './assets/traderu.gif';
+import { fadeFromLeft, fadeFromRight } from "./helpers/animations";
+import quartech from "./assets/quartech.png";
+import maximus from "./assets/maximus.jpeg";
+import softUI from "./assets/soft-ui.gif";
+import lighthouse from "./assets/lighthouse.jpeg";
+import traderu from "./assets/traderu.gif";
 
 function App() {
   return (
     <div>
       <div className="unit unit--first">
         <Reveal keyframes={fadeFromLeft}>
-          <h1>
-            Hi 👋
-          </h1>
+          <h1>Hi 👋</h1>
         </Reveal>
         <Reveal keyframes={fadeFromLeft} delay={1000}>
-          <h2>
-            I'm Bryce
-          </h2>
+          <h2>I'm Bryce</h2>
         </Reveal>
         <Reveal keyframes={fadeFromLeft} delay={2000}>
+          <h3>A software developer from Victoria, BC</h3>
+        </Reveal>
+      </div>
+      <div className="unit unit-quartech">
+        <Reveal keyframes={fadeFromRight}>
+          <div className="image-grouping">
+            <h2>Currently I'm working at Quartech</h2>
+            <img src={quartech} alt="Quartech"></img>
+          </div>
+        </Reveal>
+        <Reveal keyframes={fadeFromRight} delay={500}>
           <h3>
-            A software developer from Victoria, BC
+            I am a TypeScript & React Native developer on the BC Digital Trust
+            team
           </h3>
         </Reveal>
       </div>
       <div className="unit unit-maximus">
-        <Reveal keyframes={fadeFromRight}>
+        <Reveal keyframes={fadeFromLeft}>
           <div className="image-grouping">
-            <h2>
-              Currently I'm working at Maximus Canada
-            </h2>
+            <h2>Previously I worked at Maximus Canada</h2>
             <img src={maximus} alt="Maximus Canada"></img>
           </div>
         </Reveal>
-        <Reveal keyframes={fadeFromRight} delay={500}>
+        <Reveal keyframes={fadeFromLeft} delay={500}>
           <h3>
-            Working as an Application Developer, I build and maintain VueJS and React apps with NodeJS microservices
+            Working as an Application Developer, I built and maintained VueJS
+            and React apps with NodeJS microservices
           </h3>
         </Reveal>
       </div>
       <div className="unit">
-        <Reveal keyframes={fadeFromLeft}>
+        <Reveal keyframes={fadeFromRight}>
           <div className="image-grouping">
             <h2>
-              In my off-time I'm busy working on a React component library for neumorphic design called react-soft-ui 
+              In my off-time I'm busy working on a React component library for
+              neumorphic design called react-soft-ui
             </h2>
             <img src={softUI} alt="Soft UI"></img>
           </div>
         </Reveal>
-        <Reveal keyframes={fadeFromLeft} delay={500}>
+        <Reveal keyframes={fadeFromRight} delay={500}>
           <h3>
-            ...provided no one takes that npm module name by the time I get around to releasing it
+            ...provided no one takes that npm module name by the time I get
+            around to releasing it
           </h3>
         </Reveal>
       </div>
       <div className="unit">
-        <Reveal keyframes={fadeFromRight}>
+        <Reveal keyframes={fadeFromLeft}>
           <div className="image-grouping">
-            <h2>
-              Before working at Maximus I graduated from Lighthouse Labs
-            </h2>
+            <h2>Before working at Maximus I graduated from Lighthouse Labs</h2>
             <img src={lighthouse} alt="Lighthouse Labs"></img>
           </div>
         </Reveal>
-        <Reveal keyframes={fadeFromRight} delay={500}>
+        <Reveal keyframes={fadeFromLeft} delay={500}>
           <h3>
-            A great program with comprehensive content on Javascript, React, Ruby on Rails, and more
+            A great program with comprehensive content on Javascript, React,
+            Ruby on Rails, and more
           </h3>
         </Reveal>
       </div>
       <div className="unit unit--traderu">
-        <Reveal keyframes={fadeFromLeft}>
+        <Reveal keyframes={fadeFromRight}>
           <h2>
-            If you'd like to see a previous project of mine, take a look at <a href="https://github.com/bryce-mcmath/traderu">TraderU</a>
+            If you'd like to see a previous project of mine, take a look at{" "}
+            <a href="https://github.com/bryce-mcmath/traderu">TraderU</a>
           </h2>
         </Reveal>
-        <Reveal keyframes={fadeFromLeft} delay={500}>
+        <Reveal keyframes={fadeFromRight} delay={500}>
           <img src={traderu} alt="TraderU"></img>
         </Reveal>
       </div>
       <div className="unit">
-        <Reveal keyframes={fadeFromRight}>
+        <Reveal keyframes={fadeFromLeft}>
           <h2>
-            If you're looking to get ahold of me or get a copy of my resume, send an email to <a href="mailto:bryce.mcmath.work@gmail.com">bryce.mcmath.work@gmail.com</a>
+            If you're looking to get ahold of me or get a copy of my resume,
+            send an email to{" "}
+            <a href="mailto:bryce.mcmath.work@gmail.com">
+              bryce.mcmath.work@gmail.com
+            </a>
           </h2>
           <h4>If you'd just like to see my face, pop open the console.</h4>
         </Reveal>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
@@ -64,7 +63,3 @@ console.log(`
 @@@@@@@@@@@@@@@@@@@@@@@@@Lt1@L;;;iiiiiiiiiiii1fC8@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@C1:,              ;:. .;:,.;;:,,,::;;:::,,,,,
 @@@@@@@@@@@@@@@@@@@@@@@@@@@8GC@@fffftffLC008@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@fi,.                 ,,.   ::::::;;;;::,,,,,
 `);
-
-console.log('Reporting vitals:');
-reportWebVitals(console.log);
-
